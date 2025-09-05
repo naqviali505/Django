e4cc7c76b1791a24a25a2a2ab1b59f8a484b1a81
naqviali505 <naqviali505@gmail.com>
2025-09-06 02:17:16 +0500

    Blog Forgot Password Incomplete

--- djangotutorial/blog/urls.py
+++ djangotutorial/blog/urls.py
@@ -6,9 +6,17 @@ from django.views.generic import RedirectView
 app_name = "blog"
 urlpatterns = [
     path("",RedirectView.as_view(url=reverse_lazy("blog:login")),name="root_redirect"),
-    path("login/", auth_views.LoginView.as_view(template_name="blog/login_page.html",
+    path("login", auth_views.LoginView.as_view(template_name="blog/login_page.html",
     redirect_authenticated_user=True), name="login"),
-    path("register/",views.register,name="register"),
-    path("home/",views.home,name="home"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="blog:login"), name="logout"),
+    path("register",views.register,name="register"),
+    path("home",views.home,name="home"),
+    path("logout", auth_views.LogoutView.as_view(next_page="blog:login"), name="logout"),
+    path("forgot-password/", auth_views.PasswordResetView.as_view(
+        template_name="blog/forgot_password.html",
+        success_url=reverse_lazy("blog:forgot-password"),
+        subject_template_name="blog/password_reset_subject.txt",
+        email_template_name="blog/password_reset_email.html",  # email body
+    ), name="forgot-password"),
+    # path("password-reset/", auth_views.PasswordResetConfirmView.as_view(template_name="blog/password_rest_done.html"), name="password-reset"),
+
 ]
--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -26,6 +26,13 @@ SECRET_KEY = 'django-insecure-i5=7^167g*+dv6(wk2@7)wkfg^5t=h_7qvku@$d57f4w7^=6ps
 DEBUG = True
 
 ALLOWED_HOSTS = []
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "naqviali505@gmail.com"
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
+EMAIL_HOST_PASSWORD ="pnzonhvnzqiqakey"
 
 
 # Application definition
