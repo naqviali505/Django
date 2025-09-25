e2aac3a35bce1290c5df5ff8f8e8c3b22fedf86d
naqviali505 <naqviali505@gmail.com>
2025-09-25 23:55:27 +0500

    Need to Update List View

--- djangotutorial/blog/models.py
+++ djangotutorial/blog/models.py
@@ -1,27 +1,45 @@
+from django import forms
 from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs',null=True)
-
-    def __str__(self):
-        return self.title
-
-class BlogCategory(models.Model):
-    name = models.CharField(max_length=100)
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='blogs',null=True)
-
-    def __str__(self):
-        return self.name
-
 class Comment(models.Model):
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE,related_name='comments',null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='users',null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     content = models.TextField()
 
     def __str__(self):
         return self.content
 
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=100,verbose_name='Blog Category')
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(BlogCategory)
+    comments = models.ManyToManyField(Comment,blank=True)
+
+    def __str__(self):
+        return self.title
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title','content','user','categories', 'comments']
+
+
+class BlogCategoryForm(forms.ModelForm):
+    class Meta:
+        model = BlogCategory
+        fields = ['name']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content','user']
--- djangotutorial/blog/views.py
+++ djangotutorial/blog/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Blog
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(label="Email Address")
@@ -17,7 +18,6 @@ class CustomUserCreationForm(UserCreationForm):
             if commit:
                 user.save()
             return user
-
 
 def register(request):
     """User registration using Django's built-in form."""
@@ -34,4 +34,5 @@ def register(request):
 @login_required
 def home(request):
     """Homepage - requires login."""
-    return render(request, "blog/home_page.html")
+    blogs =Blog.objects.all()
+    return render(request, "blog/home_page.html",{"blogs":blogs})
